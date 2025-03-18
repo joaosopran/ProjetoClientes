bd2c43ab913bbf1316c6facfeb03e1290b11512f
joaosopran <159202685+joaosopran@users.noreply.github.com>
2025-03-18 18:23:01 -0300

    Validações no cliente e tratamento de erro da API

--- src/main/java/io/github/joaosopran/model/entity/Cliente.java
+++ src/main/java/io/github/joaosopran/model/entity/Cliente.java
@@ -2,7 +2,11 @@ package io.github.joaosopran.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
 
 import java.time.LocalDate;
 
@@ -18,9 +22,11 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150) //OBRIGATÓRIO O NOME E VAI TER ATÉ 150 CARACTERES
+    @NotBlank(message = "O nome não pode estar vazio!")
     private String nome;
 
-    @Column(nullable = false, length = 11) //OBRIGATÓRIO O CPF E VAI TER 11 CARACTERES
+    @Column(unique = true, nullable = false, length = 11) //OBRIGATÓRIO O CPF E VAI TER 11 CARACTERES
+    @NotBlank(message = "O CPF é obrigatório!")
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false) //NOME DA COLUNA NO BANCO DE DADOS
--- src/main/java/io/github/joaosopran/rest/ClienteController.java
+++ src/main/java/io/github/joaosopran/rest/ClienteController.java
@@ -2,10 +2,22 @@ package io.github.joaosopran.rest;
 
 import io.github.joaosopran.model.entity.Cliente;
 import io.github.joaosopran.model.repository.ClienteRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -21,7 +33,7 @@ public class ClienteController {
     // PARA SALVAR UM CLIENTE
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // ABORDAGEM DE CÓDIGO LIMPO E ORGANIZADO
-    public Cliente salvar(@RequestBody Cliente cliente) {
+    public Cliente salvar(@RequestBody @Valid Cliente cliente) {
         return repository.save(cliente);
     }
 
@@ -33,6 +45,7 @@ public class ClienteController {
 
     }
 
+    // PARA DELETAR UM CLIENTE
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Integer id){
@@ -44,14 +57,45 @@ public class ClienteController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    // PARA ATUALIZAR UM CLIENTE
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado){
         repository.findById(id)
-                  .map(cliente -> {
-                      clienteAtualizado.setId(cliente.getId());
-                      return repository.save(clienteAtualizado);
-                  })
-                  .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .map(cliente -> {
+                    clienteAtualizado.setId(cliente.getId());
+                    return repository.save(clienteAtualizado);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    // Captura exceções genéricas e retorna erro 500
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String, String>> handleGeneralExceptions(Exception ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Ocorreu um erro interno no servidor.");
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "CPF já cadastrado.");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
 }
+
