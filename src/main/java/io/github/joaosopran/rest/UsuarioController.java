3e06520ed01191cad12983857ea5e4fd45c59326
joaosopran <159202685+joaosopran@users.noreply.github.com>
2025-04-22 18:29:01 -0300

    API para cadastro de usuário

---
+++ src/main/java/io/github/joaosopran/rest/UsuarioController.java
@@ -0,0 +1,24 @@
+package io.github.joaosopran.rest;
+
+
+import io.github.joaosopran.model.entity.Usuario;
+import io.github.joaosopran.model.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioRepository repository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(@RequestBody @Valid Usuario usuario){
+        repository.save(usuario);
+
+    }
+}
